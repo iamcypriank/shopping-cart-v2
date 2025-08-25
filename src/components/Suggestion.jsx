fc01fd23fcd79af1560ee86c6d13a7e7c621998e
Cyprian Kujur <cyprian.k2003@gmail.com>
2025-08-25 18:59:46 +0530

    product count on navbar, loading animation

--- src/components/Suggestion.jsx
+++ src/components/Suggestion.jsx
@@ -1,10 +1,11 @@
 import { useFetchProducts } from "../hooks/useFetchProducts"
 import ProductCard from "./ProductCard";
+import Loading from "./Loading";
 
 export default function Suggestion({ id }){
     const { list, error, loading } = useFetchProducts(`products?categoryId=${id}`);
     return <>
-    { loading && <p>loading</p> }
+    { loading && <div className="mt-40"><p>{error}</p></div> }
     { error && <p>{error}</p> }
     { list && 
         list.map((product)=>{
